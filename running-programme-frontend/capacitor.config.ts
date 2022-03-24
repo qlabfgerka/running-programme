c99c4f8f9ad083bab57f3d3a9fff68647ee42182
Jan Premzl <jan.premzl16@gmail.com>
2022-03-24 20:31:07 +0100

    Initial commit

---
+++ running-programme-frontend/capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'running-programme-frontend',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
