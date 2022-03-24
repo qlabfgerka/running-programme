1c9b9e9b6933994b64d07da2941f71ea2fe7ed4b
Jan Premzl <jan.premzl16@gmail.com>
2022-03-24 21:01:30 +0100

    Create register page
    Implemented styling for register page

--- running-programme-frontend/src/app/pages/account/register/register.page.ts
+++ running-programme-frontend/src/app/pages/account/register/register.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -6,10 +7,39 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.page.scss'],
 })
 export class RegisterPage implements OnInit {
+  public registerForm: FormGroup;
 
-  constructor() { }
+  constructor(private readonly formBuilder: FormBuilder) {}
 
-  ngOnInit() {
+  ngOnInit(): void {
+    this.initForm();
   }
 
+  public get errorControl() {
+    return this.registerForm.controls;
+  }
+
+  public register(): void {}
+
+  private initForm(): void {
+    this.registerForm = this.formBuilder.group({
+      email: [
+        '',
+        [
+          Validators.required,
+          Validators.pattern('[^\\s@]+@[^\\s@]+\\.[^\\s@]+$'),
+        ],
+      ],
+      username: [
+        '',
+        [
+          Validators.required,
+          Validators.minLength(3),
+          Validators.pattern('^[a-zA-Z0-9-_]+$'),
+        ],
+      ],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+      repeat: ['', [Validators.required]],
+    });
+  }
 }
