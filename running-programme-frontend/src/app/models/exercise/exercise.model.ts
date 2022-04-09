75406ca1468519a490a09b191c27f24222ed111f
Jan Premzl <jan.premzl16@gmail.com>
2022-04-09 23:58:55 +0200

    Adding exercise (frontend)

---
+++ running-programme-frontend/src/app/models/exercise/exercise.model.ts
@@ -0,0 +1,13 @@
+import { UserDTO } from '../user/user.model';
+
+export class ExerciseDTO {
+  id?: string | undefined | null = null;
+  name: string | undefined | null = null;
+  frequency: number | undefined | null = null;
+  currentMinutes: number | undefined | null = null;
+  currentSeconds: number | undefined | null = null;
+  goalMinutes: number | undefined | null = null;
+  goalSeconds: number | undefined | null = null;
+  goalDate: Date | undefined | null = null;
+  user?: UserDTO | undefined | null = null;
+}
---
+++ running-programme-frontend/src/app/services/exercise/exercise.service.ts
@@ -0,0 +1,24 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { ExerciseDTO } from 'src/app/models/exercise/exercise.model';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ExerciseService {
+  private readonly hostname: string = 'http://localhost:3000';
+
+  constructor(private readonly httpClient: HttpClient) {}
+
+  public getExercises(): Observable<Array<ExerciseDTO>> {
+    return this.httpClient.get<Array<ExerciseDTO>>(`${this.hostname}/exercise`);
+  }
+
+  public addExercise(exercise: ExerciseDTO): Observable<ExerciseDTO> {
+    return this.httpClient.post<ExerciseDTO>(
+      `${this.hostname}/exercise`,
+      exercise
+    );
+  }
+}
